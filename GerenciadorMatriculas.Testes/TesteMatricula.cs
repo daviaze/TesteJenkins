7902364a4168ef783a4c3783cd54d7fdad9893bc
daviaze <davisilcampos@hotmail.com>
2022-09-13 10:34:58 -0300

    mais um teste

--- GerenciadorMatriculas.Testes/TesteMatricula.cs
+++ GerenciadorMatriculas.Testes/TesteMatricula.cs
@@ -5,6 +5,7 @@ namespace GerenciadorMatriculas.Testes
         [Theory]
         [InlineData("Davi", "Sandra", "Marcos", "22", "Rua Silva Jardim, N 55, Rio de Janeiro")]
         [InlineData("Bruno", "Thais", "Paulo", "26", "Rua Maria Es, N 100, Rio de Janeiro")]
+        [InlineData("Joao", "Fulana", "Fulano", "25", "Rua Astolfo Nunes, N 74, Rio de Janeiro")]
 
         public void TestaMatricula(string nome, string nomeMae, string nomePai, string idade, string endereco)
         {
